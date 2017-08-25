44132a01b005cda9b10dc139826d63cd57d39829
lbo <lbo@LBO-LAP.oradev.oraclecorp.com>
2017-08-24 20:41:00 -0500

    Attempted to clean up using Spring boot

--- SpringRestfulWebServicesCRUDExample/src/org/arpit/java2blog/controller/CountryController.java
+++ SpringRestfulWebServicesCRUDExample/src/org/arpit/java2blog/controller/CountryController.java
@@ -4,17 +4,30 @@ import java.util.List;
 
 import org.arpit.java2blog.bean.Country;
 import org.arpit.java2blog.service.CountryService;
+import org.springframework.boot.SpringApplication;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 
-@RestController
+@Controller
+@EnableAutoConfiguration
 public class CountryController {
 
 	private CountryService countryService = new CountryService();
+
+	@RequestMapping("/")
+	@ResponseBody
+	public String home() {
+		return "Hello World!";
+	}
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(CountryController.class, args);
+	}
 
 	@RequestMapping(value = "/countries", method = RequestMethod.GET, headers = "Accept=application/json")
 	public List getCountries() {
